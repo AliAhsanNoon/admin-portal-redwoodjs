64e4960f806a15a5cea6b91374ebd4ec8124f61a
aliahsannoon <alinoonciit@gmail.com>
2024-09-04 06:51:49 +0500

    graphql setup

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -18,6 +18,12 @@ export const handler = createGraphQLHandler({
   directives,
   sdls,
   services,
+  cors:{
+    origin: true,
+    methods: '*',
+    allowedHeaders: '*',
+    credentials: true,
+  },
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
